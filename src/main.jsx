eacdb52e5c813007794be07c293c9ce780274917
Anderson-Calderon <anderson.calderoncampos@gmail.com>
2023-07-25 15:22:56 -0500

    mejoras del diseño 1

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import '../dist/dist/css/AdminLTE.css'
+import '../dist/dist/css/skins/_all-skins.min.css'
+import '../dist/dist/js/adminlte.min.js'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
